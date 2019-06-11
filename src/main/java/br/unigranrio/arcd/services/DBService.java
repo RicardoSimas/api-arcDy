44a3b0ece9e290c34c99ba42afab2ac6a1a7981d
Ricardo Simas <Rsimas@unigranrio.br>
2019-06-11 17:35:26 -0300

    Mock de dados com geração randomica dos valores de corrente, tensão e data.

--- src/main/java/br/unigranrio/arcd/services/DBService.java
+++ src/main/java/br/unigranrio/arcd/services/DBService.java
@@ -1,7 +1,6 @@
 package br.unigranrio.arcd.services;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.unigranrio.arcd.domain.Monitoring;
 import br.unigranrio.arcd.repositories.MonitoringRepository;
+import br.unigranrio.arcd.resources.utils.Utils;
 
 @Service
 public class DBService {
@@ -18,13 +18,14 @@ public class DBService {
 	
 	public void instantiateTestDatabase() throws ParseException{
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Monitoring[] m = new Monitoring[50];
 		
-		Monitoring m1 = new Monitoring(null, 18.2, 6.8, sdf.parse("21/10/1995"));
-		Monitoring m2 = new Monitoring(null, 23.2, 9.6, sdf.parse("21/10/1995"));
-		Monitoring m3 = new Monitoring(null, 10.2, 4.2, sdf.parse("21/10/1995"));
-		Monitoring m4 = new Monitoring(null, 8.2, 2.7, sdf.parse("21/10/1995"));
-
-		repo.save(Arrays.asList(m1,m2,m3,m4));
+		for(int i = 0; i < m.length; i++) {
+			m[i] = new Monitoring(null, (Math.floor(Math.random() * Math.floor(10))),
+					(Math.floor(Math.random() * Math.floor(220))),
+					Utils.sqlDateToDate(Utils.generateDate(2000, 2018)));
+			
+			repo.save(Arrays.asList(m[i]));
+		}
 	}
 }
